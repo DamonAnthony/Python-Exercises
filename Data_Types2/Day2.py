6a2aded16b6de1f8c066bc104a1907f5db0103dc
Damon Anthony <damonanthony125@gmail.com>
2020-03-10 15:47:01 +0200

    DataTypeExercise2

---
+++ Data_Types2/Day2.py
@@ -0,0 +1,20 @@
+strSentence = "LifeChoices Coding Academy"
+print(type(strSentence))
+print(strSentence[2:])
+Number1 = 25
+Number2 = 50.5
+Number3 = 3e+7j
+print(type(Number1), type(Number2), type(Number3))
+#Sorting and inseting in list
+mylist = [65, 25, 125, 78, 120, 33]
+print(type(mylist))
+mylist.sort()
+mylist.reverse()
+mylist.insert(2, 37)
+print(mylist)
+#TupleType
+Cars = ("Datsun", "Ford", "BMW", "VW")
+Cars.sort
+#DictionaryType
+xdict = {"1": "Amy", "2": "Tania", "3": "Nathan"}
+print(type(xdict))
