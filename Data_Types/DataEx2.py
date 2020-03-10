43e593a5d160a0b83b2a2411fb8309c168ecc6fa
Damon Anthony <damonanthony125@gmail.com>
2020-03-10 15:38:43 +0200

    DataTypeExercise

---
+++ Data_Types/DataEx2.py
@@ -0,0 +1,8 @@
+Numbers = [56, 78, 34, 21, 56, 34, 125, 45, 89, 75, 12, 56]
+print(Numbers)
+A = sum(Numbers)
+print(A)
+Numbers.sort()
+print((min(Numbers)), (max(Numbers)))
+Dlist = list(set(Numbers))
+print(Dlist)
